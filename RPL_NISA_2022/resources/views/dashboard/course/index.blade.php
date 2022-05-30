01f27762ceb768b20633ffa20b4bfb7d0ef7a9dc
Wantufahati <wantufahatii@gmail.com>
2022-05-30 23:19:39 +0700

    tambahan dashboard

--- RPL_NISA_2022/resources/views/dashboard/course/index.blade.php
+++ RPL_NISA_2022/resources/views/dashboard/course/index.blade.php
@@ -6,6 +6,15 @@
 
 @section('content')
 	<div class="container">
+
+		@if (session('alert'))
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    {!! session('alert') !!}
+                </div>
+            </div>
+        @endif
+
 		<div class="card">
 			<div class="card-header">
 				<h6>Daftar Course</h6>
@@ -15,7 +24,7 @@
 					<thead>
 						<tr>
 							<th style="width:5%">#</th>
-							<th></th>
+							<th style="width:10%"></th>
 							<th>Course</th>
 							<th>Tanggal Enroll</th>
 							<th>Progress</th>
@@ -23,7 +32,31 @@
 						</tr>
 					</thead>
 					<tbody>
-						
+						@foreach($course as $row)
+							<tr>
+								<td>{{$loop->iteration}}</td>
+								<td>
+									<img class="img-fluid" src="/images/course/{{$row->course_image}}">
+								</td>
+								<td>
+									{{$row->course_name}}<br>
+									<small class="text-muted">Kategori : {{$row->category_name}}</small>
+								</td>
+								<td>
+									{{indonesian_date($row->enroll_date)}}
+								</td>
+								<td>
+									@if($row->is_done == '1')
+										<span class="badge bg-success text-white">Selesai</span>
+									@else
+										<span class="badge bg-primary text-white">On Progress</span>
+									@endif
+								</td>
+								<td>
+									<a href="/dashboard/enrollment/{{$row->enroll_id}}/{{$row->course_id}}" class="btn btn-sm btn-outline-primary">Lihat Course</a>
+								</td>
+							</tr>
+						@endforeach
 					</tbody>
 				</table>
 			</div>
